c0cf8a8e491fe16d326bf62b65902c9426bcf9c6
flvsantos15 <flvsantos15@gmail.com>
2025-06-03 18:32:03 -0300

    chore: add login service

--- src/app/component/seller/seller.component.ts
+++ src/app/component/seller/seller.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
@@ -10,17 +11,25 @@ import { SellerService } from '../../services/seller.service';
 @Component({
   selector: 'app-seller',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './seller.component.html',
   styleUrl: './seller.component.css',
 })
 export class SellerComponent implements OnInit {
   public loading: boolean = false;
+  public showLogin: boolean = false;
+  public authError: string = '';
   public sellerSignUpForm: FormGroup;
+  public sellerLoginForm: FormGroup;
 
   constructor(private sellerService: SellerService) {
     this.sellerSignUpForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+
+    this.sellerLoginForm = new FormGroup({
       email: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
     });
@@ -38,5 +47,28 @@ export class SellerComponent implements OnInit {
     }, 2000);
 
     this.sellerSignUpForm.reset();
+  }
+
+  onLogin(): void {
+    this.sellerService.login(this.sellerLoginForm.value);
+    this.sellerService.isLoginError.subscribe((error) => {
+      if (error) {
+        this.authError = 'Invalid email or password';
+      }
+    });
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
+    this.sellerLoginForm.reset();
+  }
+
+  openLogin(): void {
+    this.showLogin = true;
+  }
+
+  closeLogin(): void {
+    this.showLogin = false;
   }
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,9 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
-interface ISeller {
+interface ISellerSignUpRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface ISellerLoginRequest {
   name: string;
   email: string;
   password: string;
@@ -14,16 +20,16 @@ interface ISeller {
 })
 export class SellerService {
   isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoginError = new EventEmitter<boolean>(false);
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  signUp(seller: ISeller): void {
+  signUp(seller: ISellerSignUpRequest): void {
     this.http
       .post('http://localhost:3000/seller', seller)
       .subscribe((result) => {
         if (result) {
           this.isSellerLoggedIn.next(true);
-          console.log(result);
           localStorage.setItem('seller', JSON.stringify(result));
           this.router.navigate(['/seller-home']);
         }
@@ -36,5 +42,23 @@ export class SellerService {
       this.isSellerLoggedIn.next(true);
       this.router.navigate(['/seller-home']);
     }
+  }
+
+  login(seller: ISellerLoginRequest): void {
+    this.http
+      .get(
+        `http://localhost:3000/seller?email=${seller.email}&password=${seller.password}`,
+        { observe: 'response' }
+      )
+      .subscribe((result: HttpResponse<any>) => {
+        if (result && result.body && result.body.length > 0) {
+          this.isLoginError.emit(false);
+          this.isSellerLoggedIn.next(true);
+          localStorage.setItem('seller', JSON.stringify(result.body[0]));
+          this.router.navigate(['/seller-home']);
+        } else {
+          this.isLoginError.emit(true);
+        }
+      });
   }
 }
