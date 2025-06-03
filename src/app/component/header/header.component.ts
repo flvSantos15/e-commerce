5014de87c0ba12cbce743c688c95ec0cc6ac425a
flvsantos15 <flvsantos15@gmail.com>
2025-06-03 18:54:37 -0300

    chore: add path verification on header

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,17 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  public menuType: string = 'default';
   public cartItemCount: number = 0;
 
-  constructor() {
+  constructor(private router: Router) {
     this.cartItemCount = 0;
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: any) => {
+      if (event.url) {
+        const seller = localStorage.getItem('seller');
+        if (seller && event.url.includes('/seller')) {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+    console.log(this.menuType);
   }
 }
