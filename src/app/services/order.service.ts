d3db8ffe6a60c8498e33b4eb4d6d2ae144ca999a
flvSantos15 <flvsantos300@gmail.com>
2025-06-14 13:06:44 -0300

    feat: add order page

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,26 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-interface IOrderItem {
-  productId: string;
-  quantity: number;
-}
-
-interface IAddress {
-  street: string;
-  city: string;
-  state: string;
-  zipCode: string;
-}
-
-interface IOrder {
-  id: string;
-  userId: string;
-  items: IOrderItem[];
-  totalAmount: number;
-  status: 'pending' | 'completed' | 'cancelled';
-  address: IAddress;
-}
+import { Observable } from 'rxjs';
+import { IOrder } from '../interfaces/order';
 
 @Injectable({
   providedIn: 'root',
@@ -33,21 +14,21 @@ export class OrderService {
     return this.http.post('http://localhost:3000/orders', order);
   }
 
-  getOrders() {
-    return this.http.get('http://localhost:3000/orders');
+  getOrders(): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>('http://localhost:3000/orders');
   }
 
-  getOrderById(id: string) {
-    return this.http.get(`http://localhost:3000/orders/${id}`);
+  getOrderById(id: string): Observable<IOrder> {
+    return this.http.get<IOrder>(`http://localhost:3000/orders/${id}`);
   }
 
   updateOrder(id: string, order: IOrder) {
     // Criar um aviso de ordem atualizada
-    return this.http.put(`http://localhost:3000/orders/${id}`, order);
+    this.http.put(`http://localhost:3000/orders/${id}`, order);
   }
 
   deleteOrder(id: string) {
     // Criar um aviso de ordem deletada
-    return this.http.delete(`http://localhost:3000/orders/${id}`);
+    this.http.delete(`http://localhost:3000/orders/${id}`);
   }
 }
